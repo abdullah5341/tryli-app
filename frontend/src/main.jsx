5d1c0126fce785d00637844660f8d92598d8a75c
Abdullah Mahmood <abduklahmahmood313@gmail.com>
2025-07-02 18:44:21 +0500

    Add production React frontend to dist/

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,20 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App.jsx';
 
-import { Provider as AppBridgeProvider } from '@shopify/app-bridge-react'
+import { Provider as AppBridgeProvider } from '@shopify/app-bridge-react';
 
 const config = {
   apiKey: import.meta.env.VITE_SHOPIFY_API_KEY,
   host: new URLSearchParams(window.location.search).get("host"),
   forceRedirect: true,
-}
+};
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ReactDOM.render(
+  <React.StrictMode>
     <AppBridgeProvider config={config}>
       <App />
     </AppBridgeProvider>
-  </StrictMode>
-)
+  </React.StrictMode>,
+  document.getElementById('root')
+);
